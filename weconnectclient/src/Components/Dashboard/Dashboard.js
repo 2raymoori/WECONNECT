72c835c0c302660b2b1e541916df79dcc2445ca0
2raymoori <l2raymoori@gmail.com>
2022-08-05 19:51:56 +0200

    A Commit for Dashboard implementation

--- weconnectclient/src/Components/Dashboard/Dashboard.js
+++ weconnectclient/src/Components/Dashboard/Dashboard.js
@@ -6,15 +6,18 @@ import { uProfile } from "../../Actions/Profile.Action";
 
 const Dashboard = (props) => {
   const [profile, setProfile] = useState({});
+  const [loading,setLoading] = useState(true);
   const loadProfile = async () => {
     try {
       const res = await axios.get("/api/profile/me");
       // console.log(res.data.data[0].msg);
       const curProfile = res.data.data[0].msg;
+      console.log(curProfile)
       setProfile((profile) => ({
         ...profile,
         ...curProfile,
       }));
+      setLoading(false)
     } catch (error) {}
   };
 
@@ -24,7 +27,7 @@ const Dashboard = (props) => {
   }, []);
   if (props.auth.isAuthenticated) {
     //
-    return props.p.loading && props.p.msg === null ? (
+    return loading ? (
       <div>
         <img
           src="https://icon-library.com/images/loading-icon-animated-gif/loading-icon-animated-gif-7.jpg"
@@ -36,9 +39,6 @@ const Dashboard = (props) => {
       <Fragment>
         {}
         <h1 className="large text-primary">Dashboard</h1>
-        <p>
-          <i>Welcome {props.auth.user && props.auth.user.firstName}</i>
-        </p>
         {/* props.profile.profile.msg */}
         {/* props.profile.profile */}
         {profile.bio === undefined ? (
@@ -49,7 +49,103 @@ const Dashboard = (props) => {
             </Link>
           </Fragment>
         ) : (
-          <Fragment>has</Fragment>
+          <Fragment>
+                  
+      <p class="lead"><i class="fas fa-user"></i> Welcome <i>{props.auth.user && props.auth.user.firstName}</i></p>
+      <div class="dash-buttons">
+        <a href="edit-profile.html" class="btn btn-light"
+          ><i class="fas fa-user-circle text-primary"></i> Edit Profile</a
+        >
+        <a href="add-experience.html" class="btn btn-light"
+          ><i class="fab fa-black-tie text-primary"></i> Add Experience</a
+        >
+        <a href="add-education.html" class="btn btn-light"
+          ><i class="fas fa-graduation-cap text-primary"></i> Add Education</a
+        >
+      </div>
+
+      <h2 class="my-2">Experience Credentials</h2>
+
+      {profile.education.length ===0 ? (<div>
+          <h2>Sorry There is no Experience yet in your profile.</h2>
+          <h2>Kindly click on Add Experience to add an Experience</h2>
+          </div>
+        ):
+      (
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th class="hide-sm">Title</th>
+            <th class="hide-sm">Years</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {profile.experience.map(e=>{
+            return(<tr>
+            <td>{e.company}</td>
+            <td class="hide-sm">{e.title}</td>
+            <td class="hide-sm">
+              {e.from.split("T")[0]} - {e.current ?   ("Now"):(e.to.split("T")[0])}
+            </td>
+            <td>
+              <button class="btn btn-danger">
+                Delete
+              </button>
+            </td>
+          </tr>)
+          })
+        }
+        </tbody>
+      </table>)
+    }
+
+      <h2 class="my-2">Education Credentials</h2>
+      {
+        profile.education.length === 0 ? (<div>
+          <h2>Sorry There is no Experience yet in your profile.</h2>
+          <h2>Kindly click on Add Experience to add an Experience</h2>
+          </div>):(
+<table class="table">
+          <thead>
+            <tr>
+              <th>School</th>
+              <th class="hide-sm">Degree</th>
+              <th class="hide-sm">Years</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {profile.education.map(e=>{
+              return(
+                <tr>
+              <td>{e.school}</td>
+              <td class="hide-sm">{e.degree}</td>
+              <td class="hide-sm">
+                {e.from.split("T")[0]} - {e.current ?   ("Now"):(e.to.split("T")[0])}
+              </td>
+              <td>
+                <button class="btn btn-danger">
+                  Delete
+                </button>
+              </td>
+            </tr>
+              )
+            })}
+          </tbody>
+        </table>
+
+        )
+      }
+        <div class="my-2">
+            <button class="btn btn-danger">
+                <i class="fas fa-user-minus"></i>
+
+                Delete My Account
+            </button>
+          </div>
+          </Fragment>
         )}
       </Fragment>
     );
