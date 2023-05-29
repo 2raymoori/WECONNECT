14c609430cb116cc3e55b54ca3d50eb3fa62a197
2raymoori <l2raymoori@gmail.com>
2023-05-30 00:10:58 +0200

    Commit for seperating client codes from server code

--- weconnectclient/src/Actions/Alert.js
+++
@@ -1,9 +0,0 @@
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = String(Math.random()).split(".")[1];
-  dispatch({
-    type: "SET_ALERT",
-    payload: { msg, alertType, id },
-  });
-   setTimeout(() => dispatch({ type: "REMOVE_ALERT", payload: id }), 5000);
-};
-    
--- weconnectclient/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import alert from "./Alert";
-import auth from "./auth";
-import p from "./Profile.Reducer";
-import postReducer from "./Post.Reducer";
-export default combineReducers({ alert, auth, p, postReducer });
